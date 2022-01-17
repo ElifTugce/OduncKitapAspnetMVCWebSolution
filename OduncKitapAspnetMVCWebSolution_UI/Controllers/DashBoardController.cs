b1d1a209452fe0782a4c3fad386600346062ad9a
ElifTugce <eliftugcedemir.23@gmail.com>
2022-01-17 09:00:52 +0300

    Add project files.

---
+++ OduncKitapAspnetMVCWebSolution_UI/Controllers/DashBoardController.cs
@@ -0,0 +1,27 @@
+﻿using OduncKitapAspnetMVCWebSolution_BLL.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OduncKitapAspnetMVCWebSolution_UI.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        //Global Alan
+        KitapManager myKitapManager = new KitapManager();
+        // GET: DashBoard
+        public ActionResult Index()
+        {
+            ViewBag.BugunEklenenKitapSayisi =
+                myKitapManager.TumAktifKitaplariGetir()
+                .Where(x => x.KayitTarihi > DateTime.Now.AddDays(-1)
+                &&
+                 x.KayitTarihi < DateTime.Now.AddDays(1)
+                )
+                .Count();
+            return View();
+        }
+    }
+}
---
+++ OduncKitapAspnetMVCWebSolution_UI/Controllers/PartialsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OduncKitapAspnetMVCWebSolution_BLL.Managers;
+
+namespace OduncKitapAspnetMVCWebSolution_UI.Controllers
+{
+    public class PartialsController : Controller
+    {
+
+        //GLOBAL ALAN
+        KitapManager myKitapManager = new KitapManager();
+
+        //Partial kısmi görünüm anlamına gelmektedir.
+        //Controller içinde partiallar oluşturulabilir.
+        //Tüm partialları tek bir yerden yönetmek amacıyla PartialsController oluşturduk.
+        //Mevcutta var olan controllerların içinde de partial oluşturulabilir.
+        public PartialViewResult MenuPartialResult()
+        {
+            int toplamKitapSayisi = myKitapManager.TumAktifKitaplariGetir().Count();
+            TempData["ToplamKitapSayisi"] = toplamKitapSayisi;
+            // Umut'un viewbag dene
+            return PartialView("_PartialMenu");
+        }
+
+    }
+}
